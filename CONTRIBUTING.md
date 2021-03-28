f5171ee6c18ccc04459d5e6106d7a5c95580bfed
Adithya-adi-Menon <53041550+Adithya-adi-Menon@users.noreply.github.com>
2021-03-28 16:58:29 +0530

    huepal

---
+++ CONTRIBUTING.md
@@ -0,0 +1,26 @@
+This is a [Chrome Extension](https://developer.chrome.com/extensions/devguide).
+
+Please feel free to submit Pull Requests!
+
+
+File Structure
+---
+
+```
++-- filters/ -> the filter Javascript files
+|
++-- images/ -> the logo and and icons
+|
++-- popup/
+|   +-- popup.html -> the extension's popup html structure
+|
++-- screenshots/
+|
++-- script/
+|   +-- background.js -> mandatory file, currently empty
+|   +-- colorblindListener.js -> popup event and click handlers <-- this is where the magic happens
+|   +-- hover.js -> adds hover effects to the popup window so users can see a preview of the filter
+|
++-- style/ -> CSS stylesheets
+...
+```
---
+++ filters/deuteranomaly.js
@@ -0,0 +1,21 @@
+// deuteranomaly
+
+if (document.getElementById("styleID612481")) {
+    stylingID = document.getElementById("styleID612481").remove();
+    filterID = document.getElementById("filterID471924").remove();
+}
+stylingID = document.createElement('style');
+stylingID.id = "styleID612481";
+document.body.appendChild(stylingID);
+
+filterID = document.createElement('div');
+filterID.id = "filterID471924";
+filterID.setAttribute('style', 'height: 0; padding: 0; margin: 0; line-height: 0;');
+document.body.appendChild(filterID);
+
+filterID.innerHTML = '<svg id="colorblind-filters" style="display: none"> <defs> <filter id="deuteranomaly"> <feColorMatrix type="matrix" values="0.8,0.2,0,0,0 0.258,0.742,0,0,0 0,0.142,0.858,0,0 0,0,0,1,0" in="SourceGraphic" /> </filter> </defs> </svg>';
+stylingID.innerHTML = 'html{-webkit-filter:url(#deuteranomaly);-moz-filter:(#deuteranomaly);-ms-filter:(#deuteranomaly);-o-filter:(#deuteranomaly);filter:(#deuteranomaly);}'
+setTimeout(function() {
+    window.scrollBy(1, 1);
+    window.scrollBy(-1, -1);
+}, 1);
